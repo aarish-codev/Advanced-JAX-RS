ed74f9b834ffc3c4e692c3cf2f31ffc9e1689ea2
aarishS <aarish.work@gmail.com>
2019-05-18 22:39:40 +0530

    path&query Params

--- adv-jax-rs/src/main/java/org/aarish/learning/rest/MyResource.java
+++ adv-jax-rs/src/main/java/org/aarish/learning/rest/MyResource.java
@@ -1,20 +1,25 @@
 package org.aarish.learning.rest;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("api")
-@Singleton
+@Path("{pathParam}/api")
 public class MyResource {
 	
-	int count = 0;
+	@PathParam("pathParam")
+	private String pathParam;
+	
+	@QueryParam("query")
+	private String queryParam;
+	
+	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getData() {
-		count++;
-		return "No of times visited: "+count;
+		return "Path param: "+pathParam+" and Query param: "+queryParam;
 	}
 }
