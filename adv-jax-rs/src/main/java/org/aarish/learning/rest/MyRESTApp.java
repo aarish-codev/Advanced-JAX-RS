a7a3eaabd06a26ed6e69befdeee607208908c566
aarishS <aarish.work@gmail.com>
2019-05-18 19:08:38 +0530

    setting up application

---
+++ adv-jax-rs/src/main/java/org/aarish/learning/rest/MyRESTApp.java
@@ -0,0 +1,9 @@
+package org.aarish.learning.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webapi")
+public class MyRESTApp extends Application{
+
+}
